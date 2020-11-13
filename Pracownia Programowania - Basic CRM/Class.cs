93054907806097d4aadfa3940233d5e82f01e518
Jan Majchrzycki <majchrzycki2000@gmail.com>
2020-11-13 19:16:32 +0100

    class created

---
+++ Pracownia Programowania - Basic CRM/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pracownia_Programowania___Basic_CRM
+{
+    public class Class
+    {
+
+        //siemaneczko
+    }
+}
