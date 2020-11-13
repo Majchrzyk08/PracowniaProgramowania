5135b47e577a83dbbf0b3cfa2063fc05d370ef9d
Jan Majchrzycki <majchrzycki2000@gmail.com>
2020-11-13 19:00:55 +0100

    First Time Commit

---
+++ Pracownia Programowania - Basic CRM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pracownia_Programowania___Basic_CRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
